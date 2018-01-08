233be0568ea0dc078c885fa78f3107731b83f567
unknown <lynam.chris@gmail.com>
2018-01-08 13:13:11 -0600

    added JawsDB connection information for Heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
-
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
