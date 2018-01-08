cc4de362d8acf8cd8403c781e42366cd8958b9ad
unknown <lynam.chris@gmail.com>
2018-01-08 12:26:32 -0600

    raw functionality has been acheived

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,94 @@
-var connection = require("./connection.js");
+// Import the MySQL connection object
+var connection = require ('./connection.js');
 
-// Object Relational Mapper (ORM)
+// Helper function for generating MySQL syntax
+function printQuestionMarks(num) {
+	var arr = [];
 
+	for (var i = 0; i < num; i++) {
+		arr.push("?");
+	}
+
+	return arr.toString();
+}
+
+// Helper function for generating My SQL syntax
+function objToSql(ob) {
+	var arr = [];
+
+	for (var key in ob) {
+		arr.push(key + "=" + ob[key]);
+	}
+
+	return arr.toString();
+}
+
+// Create the ORM object to perform SQL queries
 var orm = {
-  selectAll: function(table) {
-    var queryString = "SELECT * FROM ??";
-    connection.query(queryString, [table], function(err, result) {
-      console.log(result);
-    });
-  },
-  insertOne: function(table, column, value) {
-    var queryString = "INSERT INTO ?? (??) VALUES (??)";
-    console.log(queryString);
-    connection.query(queryString, [table, column, value], function(err, result) {
-      console.log(result);
-    });
-  },
-  updateOne: function(table, columnOne, valueOne, columnTwo, valueTwo) {
-    var queryString = "UPDATE ?? SET ?? = ?? WHERE ?? = ?";
+	// Function that returns all table entries
+	selectAll: function(tableInput, cb) {
+		// Construct the query string that returns all rows from the target table
+		var queryString = "SELECT * FROM " + tableInput + ";";
 
-    connection.query(queryString, [table, columnOne, valueOne, columnTwo, valueTwo], function(err, result) {
-      console.log(result);
-    });
-  }
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+	// Function that insert a single table entry
+	insertOne: function(table, cols, vals, cb) {
+		// Construct the query string that inserts a single row into the target table
+		var queryString = "INSERT INTO " + table;
+
+		queryString += " (";
+		queryString += cols.toString();
+		queryString += ") ";
+		queryString += "VALUES (";
+		queryString += printQuestionMarks(vals.length);
+		queryString += ") ";
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, vals, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	},
+
+	// Function that updates a single table entry
+	updateOne: function(table, objColVals, condition, cb) {
+		// Construct the query string that updates a single entry in the target table
+		var queryString = "UPDATE " + table;
+
+		queryString += " SET ";
+		queryString += objToSql(objColVals);
+		queryString += " WHERE ";
+		queryString += condition;
+
+		// console.log(queryString);
+
+		// Perform the database query
+		connection.query(queryString, function(err, result) {
+			if (err) {
+				throw err;
+			}
+
+			// Return results in callback
+			cb(result);
+		});
+	}
 };
 
+// Export the orm object for use in other modules
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,30 @@
-//Models
 
-var orm = require("../config/orm.js");
+// Import the ORM to implement functions that will interact with the database
+var orm = require('../config/orm.js');
 
-orm.selectAndOrder("animal_name", "pets", "price");
+// Create the burger object
+var burger = {
+  // Select all burger table entries
+  selectAll: function(cb) {
+    orm.selectAll('burgers', function(res) {
+      cb(res);
+    });
+  },
+
+  // The variables cols and vals are arrays
+  insertOne: function(cols, vals, cb) {
+    orm.insertOne('burgers', cols, vals, function(res) {
+      cb(res);
+    });
+  },
+
+  // The objColVals is an object specifying columns as object keys with associated values
+  updateOne: function(objColVals, condition, cb) {
+    orm.updateOne('burgers', objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+
+// Export the database functions for the controller (burgerController.js).
+module.exports = burger;
